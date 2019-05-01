da183f71e3a7065ff1e48f59e159047195214e67
ach <achrefelmansour.kahlani@esprit.tn>
2019-05-01 19:44:25 +0100

    Facebook Login

--- Pets/ViewController.swift
+++ Pets/ViewController.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import FBSDKCoreKit
+import FBSDKLoginKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var _username: UITextField!
@@ -31,5 +32,38 @@ class ViewController: UIViewController {
         performSegue(withIdentifier: "signup", sender: self)
     }
     
+    @IBAction func fbLogin(_ sender: Any) {
+        let fbLoginManager:FBSDKLoginManager = FBSDKLoginManager()
+        fbLoginManager.logIn(withReadPermissions: ["email"], from: self){ (result , error ) in
+            if (error == nil){
+                let fbLoginResult:FBSDKLoginManagerLoginResult = result!
+                if fbLoginResult.grantedPermissions != nil {
+                    print("logged in")
+                    if (fbLoginResult.grantedPermissions.contains("email")){
+                        print ("granted")
+                        self.getUserData()
+                        fbLoginManager.logOut()
+                    }
+                }
+            }
+            
+        }
+    }
+    func fbButtonDidLogout(_ loginButton : FBSDKLoginButton){
+        
+    }
+    func getUserData(){
+        if (FBSDKAccessToken.current() != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields" : "email"])
+                .start(completionHandler: {(connection , result , error) -> Void in
+                    if (error == nil){
+                        let faceDic = result as! [String:AnyObject]
+                        print (faceDic)
+                        let email = faceDic["email"] as! String
+                        print (email)
+                    }
+                })
+        }
+    }
 }
 
