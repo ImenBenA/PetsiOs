9293b87b86e2e5b7fe5f7fd5c61726302890206e
fedichebbi <35900204+fedichebbi@users.noreply.github.com>
2019-05-15 02:24:57 +0100

    add post (upload image included)

--- Pets/ViewController.swift
+++ Pets/ViewController.swift
@@ -5,13 +5,12 @@
 //  Created by ESPRIT on 10/04/2019.
 //  Copyright © 2019 ESPRIT. All rights reserved.
 //
-
 import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
 import Alamofire
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var _username: UITextField!
     @IBOutlet weak var _password: UITextField!
     @IBOutlet weak var _login_button: UIButton!
@@ -20,7 +19,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     @IBAction func login_action(_ sender: Any) {
         let username = _username.text
         let password = _password.text
@@ -37,15 +36,17 @@ class ViewController: UIViewController {
         
         Alamofire.request(request).responseJSON {
             (response) in
-            if (true){
-            let user = response.result.value as! Dictionary<String,Any>
-            let usr = user["username"] as! String
-            self.loginSuccess()
+            let user = response.result.value as! Dictionary<String,String>
+            if (user != Dictionary<String,String>()){
+                let usr = user["username"] as! String
+                print(usr)
+                self.loginSuccess()
             }
+            else {print("wrong data")}
         }
     }
     
-
+    
     @IBAction func signup_action(_ sender: Any) {
         performSegue(withIdentifier: "signup", sender: self)
     }
@@ -88,4 +89,3 @@ class ViewController: UIViewController {
         }
     }
 }
-
